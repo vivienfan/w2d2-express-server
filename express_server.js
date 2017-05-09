8e31317fc8d67e6758f593a462f6bada38270f03
vagrant <vivien.fan.1224@gmail.com>
2017-05-09 17:25:58 +0000

    generate short url

--- express_server.js
+++ express_server.js
@@ -1,14 +1,36 @@
+// Importing modules
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+// Enviornment setup
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Global variable
+const PORT = process.env.PORT || 8080; // default port 8080
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.set('view engine', 'ejs');
+// Helper function
+function generateRandomString() {
+  let newURL = "";
+  const dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+  // generate a new shortURL
+  // if it already exists, generate again
+  do {
+    for (let i = 0; i < 6; i++ ) {
+      newURL += dictionary.charAt(
+        Math.round(Math.random() * dictionary.length));
+    }
+  } while(urlDatabase[newURL])
+  return newURL;
+}
+
+// Request responses:
 app.get("/", (req, res) => {
   res.end("Hello!\n");
 });
@@ -22,14 +44,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id , urls: urlDatabase};
   res.render("urls_show", templateVars);
 });
 
-
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL);
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.send("Ok");
 });
 
 app.listen(PORT, () => {
